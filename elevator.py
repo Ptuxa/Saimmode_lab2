44b2e069caeb018b857c84f914973c06a5cbbb01
Stanislav Naftolski <89383394+Ptuxa@users.noreply.github.com>
2024-10-24 13:43:20 +0300

    feat: add limits for simulation and add responses (#2)
    * refactor: add in README file info about responses
    
    * fix: change limit simulation for floors and elevators

--- elevator.py
+++ elevator.py
@@ -1,6 +1,5 @@
 class Elevator:
-    def __init__(self, env, elevator_id, capacity, travel_time, load_time, floors):
-        self.env = env
+    def __init__(self, elevator_id, capacity, travel_time, load_time, floors, all_time):
         self.id = elevator_id
         self.capacity = capacity
         self.travel_time = travel_time
@@ -9,32 +8,38 @@ class Elevator:
         self.direction = 0
         self.total_trips = 0
         self.passengers = []
+        self.total_passengers_all_time = 0 
+        self.total_waiting_pass_destination = 0
         self.floors = floors
         self.timer_counter = 0
+        self.all_time = all_time
 
     def run(self):
         """Основная логика работы лифта"""
+        
+        
+        
         # Если лифт стоит (direction = 0), ищем ближайший этаж с запросом
         if self.direction == 0:
             next_floor = self.find_closest_request()
             if next_floor is not None:
                 # Если следующий этаж не текущий, просто перемещаемся без загрузки
                 if next_floor != self.current_floor:
-                    self.move_to_floor(next_floor)
-                    self.load_unload_passengers()
+                    if self.move_to_floor(next_floor) == 0: return                  
+                    if self.load_unload_passengers() == 0: return
                 else:
                     # Если запрос на текущем этаже, загружаем/выгружаем пассажиров
-                    self.load_unload_passengers()
-            # else:
-                # self.timer_counter = self.timer_counter + 1
+                    if self.load_unload_passengers() == 0: return
+            else:
+                return 1
         else:
             # Поиск следующего этажа
             next_floor = self.find_next_request()
             
             if next_floor is not None:
                 # Лифт движется в направлении, если есть запросы на текущем этаже
-                self.move_to_floor(next_floor)
-                self.load_unload_passengers()
+                if self.move_to_floor(next_floor) == 0: return
+                if self.load_unload_passengers() == 0: return
 
     def find_closest_request(self):
         """Поиск ближайшего этажа с запросом"""
@@ -73,21 +78,32 @@ class Elevator:
         self.current_floor = target_floor
         print(f"Лифт {self.id + 1} прибыл на этаж {self.current_floor + 1} в момент времени {self.timer_counter}")
         print()
-        
+        if self.timer_counter >= self.all_time:
+            return 0
+                
     def load_unload_passengers(self):
         """Загрузка и выгрузка пассажиров"""
         print(f"Лифт {self.id + 1} выгружает пассажиров на {self.current_floor + 1} этаж в момент времени {self.timer_counter}")
         
         print(f"До выгрузки в лифте {self.id + 1} было {len(self.passengers)} человек")
         # Выгрузка пассажиров на нужном этаже
-        self.passengers = [p for p in self.passengers if p[0] != self.current_floor]
+        
+        for passengerInfo in self.passengers[:]:
+            if passengerInfo[0] == self.current_floor:
+                self.total_waiting_pass_destination = self.total_waiting_pass_destination + (self.timer_counter - passengerInfo[1])
+                self.passengers.remove(passengerInfo)                
+        
+        # self.passengers = [p for p in self.passengers if p[0] != self.current_floor]
         print(f"После выгрузки в лифте {self.id + 1} стало {len(self.passengers)} человек")
 
         # Загрузка новых пассажиров с очереди
         current_floor_requests = self.floors[self.current_floor].take_requests_before_time(self.timer_counter)[self.direction_to_str()]
         while current_floor_requests and len(self.passengers) < self.capacity:
-            target_floor = current_floor_requests.pop(0)            
-            self.passengers.append(target_floor)
+            self.total_passengers_all_time = self.total_passengers_all_time + 1
+            values = current_floor_requests.pop(0)
+            print(f"target floor: {values[0] + 1}")
+            print(f"timer_marker: {values[1]}")
+            self.passengers.append((values[0], values[1]))
         
         self.floors[self.current_floor].requests[self.direction_to_str()] = current_floor_requests
             
@@ -95,6 +111,9 @@ class Elevator:
         print()
 
         self.timer_counter = self.timer_counter + self.load_time
+        
+        if self.timer_counter >= self.all_time:
+            return 0
 
     def find_next_request(self):
         
@@ -119,3 +138,10 @@ class Elevator:
             
         self.direction = 0
         return None
+    
+    def increase_timer_counter(self, timestamp):
+        if (timestamp <= self.all_time):
+            self.timer_counter = timestamp
+        else:
+            self.timer_counter = self.all_time
+        
--- program.py
+++ program.py
@@ -6,7 +6,7 @@ from elevetorSystem import ElevatorSystem
 NUM_ELEVATORS = 2
 TRAVEL_TIME = 3 / 60  # Время движения лифта от этажа до этажа (мин)
 LOAD_TIME = 5 / 60  # Время загрузки лифта (мин)
-FLOOR_FREQUENCY = [3, 1, 1, 2, 2, 2, 2, 2, 2]  # Частота пассажиров на каждом этаже (в мин)
+FLOOR_FREQUENCY = [0.7, 0.1, 0.1, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2]  # Частота пассажиров на каждом этаже (в мин)
 CAPACITIES = [6, 8]  # Вместимость лифтов
 PROBABILITY_MATRIX = [
     [0, 0.1, 0.1, 0.1, 0.1, 0.2, 0.2, 0.1, 0.1],  # The 1st floor -> to every other floor. 
@@ -22,13 +22,11 @@ PROBABILITY_MATRIX = [
 TIME_LIMIT = 5  # Ограничение по времени (мин)
 NUMBER_OF_RUNS = 1
 
-# Запуск симуляции
-env = simpy.Environment()
-elevator_system = ElevatorSystem(env, num_elevators=NUM_ELEVATORS, capacities=CAPACITIES, travel_time=TRAVEL_TIME, load_time=LOAD_TIME, floor_frequency=FLOOR_FREQUENCY, probability_matrix=PROBABILITY_MATRIX, time=TIME_LIMIT, number_of_runs=NUMBER_OF_RUNS)
+elevator_system = ElevatorSystem(num_elevators=NUM_ELEVATORS, capacities=CAPACITIES, travel_time=TRAVEL_TIME, load_time=LOAD_TIME, floor_frequency=FLOOR_FREQUENCY, probability_matrix=PROBABILITY_MATRIX, time=TIME_LIMIT, number_of_runs=NUMBER_OF_RUNS)
 
 print()
 elevator_system.start()
 print()
 print()
 
-elevator_system.report_statistics(TIME_LIMIT)
+elevator_system.report_statistics()
